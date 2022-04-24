47aa652051c283b38abe6cd6946a829d5f71fe0b
VP <huuwulleop@hotmail.com>
2022-04-24 04:58:16 -0400

    Add colly boilerplates

--- src/main.go
+++ src/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+type Item struct {
+	Name   string `json:"name"`
+	Author string `json:"price"`
+	Url    string `json:"url"`
+}
 
 func main() {
 	fmt.Println("Test")
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("forums.rpgmakerweb.com"),
+	)
+
+	c.OnHTML("div[data-author=IMP1]", func(h *colly.HTMLElement) {
+		fmt.Println(h.Text)
+	})
+
+	c.Visit("https://forums.rpgmakerweb.com/index.php?forums/rgss3-scripts-rmvx-ace.35/")
 }
